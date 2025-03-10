6c11cb5f2bfa1a4628d3aee22842bbe05c32c19c
Kamil Molendys <1657091+ilkamo@users.noreply.github.com>
2025-03-10 21:28:14 +0100

    feat: support jupiter new hostnames

--- jupiter/api.go
+++ jupiter/api.go
@@ -2,7 +2,8 @@ package jupiter
 
 const (
 	// DefaultAPIURL is the default Jupiter API provided by the official Jupiter team.
-	DefaultAPIURL = "https://quote-api.jup.ag/v6"
+	// For more info visit: https://station.jup.ag/docs/
+	DefaultAPIURL = "https://api.jup.ag/swap/v1"
 
 	// JupiterAPIURL provides higher rate limits, but includes a small 0.2% platform fee.
 	JupiterAPIURL = "https://public.jupiterapi.com"
