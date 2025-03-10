682a1dcca99b3f30a5f36472003d7c0cb9b0c198
Kamil Molendys <1657091+ilkamo@users.noreply.github.com>
2025-03-10 21:34:24 +0100

    feat: support jupiter new hostnames (#37)
    * feat: support jupiter new hostnames
    
    * added LegacyAPIURL

--- jupiter/api.go
+++ jupiter/api.go
@@ -2,7 +2,12 @@ package jupiter
 
 const (
 	// DefaultAPIURL is the default Jupiter API provided by the official Jupiter team.
-	DefaultAPIURL = "https://quote-api.jup.ag/v6"
+	// For more info visit: https://station.jup.ag/docs/
+	DefaultAPIURL = "https://api.jup.ag/swap/v1"
+
+	// LegacyAPIURL is the legacy Jupiter API provided by the official Jupiter team.
+	// Old hostnames will be fully deprecated on 1 June 2025.
+	LegacyAPIURL = "https://quote-api.jup.ag/v6"
 
 	// JupiterAPIURL provides higher rate limits, but includes a small 0.2% platform fee.
 	JupiterAPIURL = "https://public.jupiterapi.com"
