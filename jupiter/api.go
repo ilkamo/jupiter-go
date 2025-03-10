5c2f79c71d9cd9d4360dbc20176fc134a74eb1f1
Kamil Molendys <1657091+ilkamo@users.noreply.github.com>
2025-03-10 21:33:24 +0100

    added LegacyAPIURL

--- jupiter/api.go
+++ jupiter/api.go
@@ -5,6 +5,10 @@ const (
 	// For more info visit: https://station.jup.ag/docs/
 	DefaultAPIURL = "https://api.jup.ag/swap/v1"
 
+	// LegacyAPIURL is the legacy Jupiter API provided by the official Jupiter team.
+	// Old hostnames will be fully deprecated on 1 June 2025.
+	LegacyAPIURL = "https://quote-api.jup.ag/v6"
+
 	// JupiterAPIURL provides higher rate limits, but includes a small 0.2% platform fee.
 	JupiterAPIURL = "https://public.jupiterapi.com"
 )
