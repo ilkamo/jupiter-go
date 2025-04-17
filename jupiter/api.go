bc6ff4bf394299c940a52f6a7552b984ff7bdab4
Kamil Molendys <1657091+ilkamo@users.noreply.github.com>
2025-04-17 17:47:55 +0200

    feat: migrate default api to lite-api.jup.ag

--- jupiter/api.go
+++ jupiter/api.go
@@ -2,6 +2,6 @@ package jupiter
 
 const (
 	// DefaultAPIURL is the default Jupiter API provided by the official Jupiter team.
-	// For more info visit: https://station.jup.ag/docs/
-	DefaultAPIURL = "https://api.jup.ag/swap/v1"
+	// For more info visit: https://dev.jup.ag/docs#whats-new
+	DefaultAPIURL = "https://lite-api.jup.ag/swap/v1"
 )
