c4ca97eb05090ca29b20612e003cacff35bf9526
Ben Wolfaardt <ben.wolfaardt@kima.finance>
2026-02-17 12:09:59 +0200

    Fix missing line

--- jupiter/api.go
+++ jupiter/api.go
@@ -2,5 +2,6 @@ package jupiter
 
 const (
 	// DefaultAPIURL is the default Jupiter API provided by the official Jupiter team.
+	// For more info visit: https://dev.jup.ag/docs#whats-new
 	DefaultAPIURL = "https://api.jup.ag/swap/v1"
 )
